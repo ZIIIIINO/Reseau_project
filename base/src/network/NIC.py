53f045f0d8a602a779434e150b304742f18ca9d2
ZIIIIINO <232034@umons.ac.be>
2026-04-09 08:01:39 +0200

    Work  on NIC

--- base/src/network/NIC.py
+++ base/src/network/NIC.py
@@ -5,11 +5,11 @@ from simulator.SimulatedEntity import SimulatedEntity
 
 
 class NIC(SimulatedEntity):
-    queue = Queue()
     rate = None
     queue_size = None
     sim = None
     name = None
+    host = None
 
     def __init__(self, sim: Simulator, name: str, rate: float, queue_size: int = 0):
         super().__init__(sim)
@@ -17,3 +17,14 @@ class NIC(SimulatedEntity):
         self.queue_size = queue_size
         self.sim = sim
         self.name = name
+
+        self.queue = Queue(queue_size)
+
+    def set_host(self, host):
+        self.host = host
+
+    def get_host(self):
+        return self.host
+
+    def queue_depth(self):
+        return self.queue.nbrCurrent
