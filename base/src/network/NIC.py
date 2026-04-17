360488fe43e145dc1a263a4eca040594a1870652
ZIIIIINO <232034@umons.ac.be>
2026-04-17 15:01:42 +0200

    Fix: Delay de propagation test

--- base/src/network/NIC.py
+++ base/src/network/NIC.py
@@ -8,35 +8,37 @@ from simulator.Event import Event
 class NIC(SimulatedEntity):
     def __init__(self, sim: Simulator, name: str, rate: float, queue_size: int = 0):
         self.link = None
-        self.host= None
+        self.host = None
         self.occupied = False
         super().__init__(sim)
         self.rate = rate
-        self.queue_size = 10
+        self.queue_size = queue_size
         self.sim = sim
         self.name = name
 
         self.queue = []
 
-
     def send(self, pkt: Packet):
 
         if len(self.queue) <= self.queue_size:
-             self.queue.append(pkt)
+            self.queue.append(pkt)
 
         self.sendTry()
-    def sendTry(self):
 
+    def sendTry(self):
         if len(self.queue) != 0:
             pkt = self.queue.pop(0)
-
-            if (self.occupied == False) and (self.link !=None):
-                tTransmission = pkt.size * self.rate
+            if (self.occupied == False) and (self.link != None):
+                # Car envoie en bits
+                tTransmission = (pkt.size / self.rate) * 8
+                print(tTransmission)
                 tPropagation = self.link.distance / self.link.speed
-                self.sim.add_event(Event(pkt, self.sendPacket)  , tTransmission+tPropagation)
+                print(tPropagation)
+                self.sim.add_event(
+                    Event(pkt, self.sendPacket), tTransmission + tPropagation
+                )
                 self.sim.add_event(Event(None, self.checkup), tPropagation)
-                self.occupied = True 
-
+                self.occupied = True
 
     def sendPacket(self, pkt: Packet):
         self.link.other(self).receive(pkt)
--- base/src/simulator/Simulator.py
+++ base/src/simulator/Simulator.py
@@ -14,7 +14,6 @@ class Simulator:
 
     def add_event(self, event, delta_t):
         self.__logger.debug(f"Simulator queueing event {event} in {delta_t} s")
-        print(delta_t)
         assert delta_t >= 0
         self.q.put(SimulatorEvent(self.__now + delta_t, Simulator._en, event))
         Simulator._en += 1
